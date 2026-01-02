93a33cd97349a6fcebd0992ebb8383bfc003d721
Ejona <ejone.humolli@student.uni-pr.edu>
2026-01-02 22:49:24 +0100

    Added Person Structure and showDestination function

--- Grupi47/main.cpp
+++ Grupi47/main.cpp
@@ -4,3 +4,26 @@ using namespace std;
 
 
 
+						//New code on feature-ejona branch
+struct Person {
+
+	string name;
+	char chosenDestination[5]; //max 5 destinations
+	int days[5];  //max 5 days
+	char transport[5]; //F-Flight, T-Train, B-Bus
+	char priority[5]; //H-high, m-medium, l-low
+	int numChosen;
+
+};
+
+//Function-Show the list of destinations
+void showDestinations() {
+
+	cout << "n\Available destinations:\n";
+	cout << "N-New York\n";
+	cout << "L-London\n";
+	cout << "P-Paris\n";
+	cout << "R-Rome\n";
+	cout << "T-Tokyo\n";
+
+}
